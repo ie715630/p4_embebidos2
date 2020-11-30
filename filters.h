be0d58968b7f88597cffe05a7a8be123db5e750b
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2020-11-30 01:26:04 -0600

    Added definitions

--- filters.h
+++ filters.h
@@ -9,8 +9,13 @@
 #define P4_EMBEBIDOS2_FILTERS_H_
 
 #include "arm_math.h"
+#include "stdio.h"
+#include "stdint.h"
 
+#define BLOCK_SIZE            32
+#define NUM_TAPS              1
 
+const float32_t firCoeffs[NUM_TAPS] = {0};
 
 
 #endif /* P4_EMBEBIDOS2_FILTERS_H_ */
