7053d1d1ccde1bbd40e4fb2e848da47c1adb5a80
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2020-11-30 13:10:44 -0600

    Added FIR call function

--- filters.c
+++ filters.c
@@ -21,3 +21,8 @@ void initialize_fir(arm_fir_instance_f32* S, float32_t* coeffs, float32_t* state
 {
 	arm_fir_init_f32(S, NUM_TAPS, coeffs, state, blockSize);
 }
+
+void call_fir(arm_fir_instance_f32* S, float32_t* pts_src, float32_t* ptr_dst)
+{
+	arm_fir_f32(S, ptr_src, ptr_dst, blockSize);
+}
--- filters.h
+++ filters.h
@@ -21,4 +21,6 @@ const float32_t firCoeffs[NUM_TAPS] = {0};
 
 void initialize_fir(arm_fir_instance_f32* S, float32_t* coeffs, float32_t* state);
 
+void call_fir(arm_fir_instance_f32* S, float32_t* pts_src, float32_t* ptr_dst);
+
 #endif /* P4_EMBEBIDOS2_FILTERS_H_ */
